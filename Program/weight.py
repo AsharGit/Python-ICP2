4a6c992b3f3aac45f69f9897c3d7d7e6cbb80f16
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-06-12 13:13:51 -0500

    ICP update
    ICP update

--- Program/weight.py
+++ Program/weight.py
@@ -4,19 +4,29 @@ blist = []
 # Get number of students from user
 num = int(input("How many students?:"))
 
-# Random weight generated for students
+# Ask user for students weight
 for i in range(num):
     weight = int(input("Please enter weight in lbs:"))
     alist.append(weight)
 
 # Print weight in lbs and convert to kg for another list
+# Using loop:
 print("\nWeights in pounds:")
 for i in alist:
     print(str(i) + " lbs")
     lbstokg = round((i/2.205), 2)
     blist.append(lbstokg)
 
+# Using list comprehension:
+clist = [round((i/2.205), 2) for i in alist]
+
 # Print weight in kg
-print("\nWeights in kgs:")
+# Loop
+print("\nWeights in kgs using loop:")
 for i in blist:
+    print(str(i) + " kg")
+
+# List comprehension
+print("\nWeights in kgs using list comprehension:")
+for i in clist:
     print(str(i) + " kg")
