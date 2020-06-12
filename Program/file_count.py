9e02c45549c153827ce5cfa1288ff9d865aec788
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-06-12 12:29:51 -0500

    ICP update
    ICP update

--- Program/file_count.py
+++ Program/file_count.py
@@ -1,11 +1,18 @@
 d = {}
-file = open("test.txt")
+
+# Read file to get data
+file = open('test.txt', 'r')
 data = file.read()
 words = data.split()
+file.close()
 
+# Add word and count to dictionary
 for i in words:
     d[i] = d.get(i, 0) + 1
 
-print(d)
+# Write dictionary to file
+file = open('test.txt', 'a')
+file.write('\n')
+file.write(str(d))
 file.close()
 
